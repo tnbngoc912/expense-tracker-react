9e068eb71d38643cd5b85bf16639dc10d65789b7
Ngoc Ton <ngocton@Ngocs-MacBook-Pro.local>
2022-02-02 14:40:06 +0700

    Expense tracker using context

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import { Header } from "./components/Header";
 import { IncomeExpense } from "./components/IncomeExpense";
 import { TransactionList } from "./components/TransactionList";
 
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +17,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
